a664f45040477d93a39dd2dcecb10a2408639088
Rudá Moura <rmoura@redhat.com>
2019-04-25 12:04:11 -0300

    Add setup.py to pipenv install conduitqe module

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""A setuptools-based script for installing conduitqe."""
+import os
+
+from setuptools import find_packages, setup
+
+_project_root = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(_project_root, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='conduitqe',
+    author='PEAQe Team',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Quality Assurance'
+    ],
+    description=(
+        'A GPL-licensed Python library that facilitates functional testing of '
+        'rhsm-conduit.'
+    ),
+    extras_require={
+        'dev': [
+            # For `make lint`
+            'flake8',
+            'flake8-docstrings',
+            'flake8-import-order',
+            'flake8-quotes',
+            # For `make test-coverage`
+            'pytest-cov',
+        ],
+    },
+    install_requires=[
+        'pytest',
+        'requests',
+    ],
+    license='GPLv3',
+    long_description=long_description,
+    packages=find_packages(include=['conduitqe*']),
+    url='https://github.com/peaqe/conduit-qe',
+)
