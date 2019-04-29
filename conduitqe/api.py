9264822cbbebb48ca94e3ebf8315e0fc6810af05
Rudá Moura <rmoura@redhat.com>
2019-04-28 23:07:55 -0300

    Switch tests to use Static Endpoints (/actuator)
    Following MR https://github.com/RedHatInsights/rhsm-conduit/pull/75

--- conduitqe/api.py
+++ conduitqe/api.py
@@ -6,22 +6,21 @@ import requests
 
 logger = logging.getLogger(__name__)
 
-CONDUIT_BASE_URL = 'http://localhost:8080/rhsm-conduit'
-CONDUIT_API_VERSION = 'v1'
-CONDUIT_API_URL = f'{CONDUIT_BASE_URL}/{CONDUIT_API_VERSION}'
-CONDUIT_API_URL_STATUS = f'{CONDUIT_API_URL}/status'
-CONDUIT_API_URL_AVAILABILITY = f'{CONDUIT_API_URL}/status/ready'
+CONDUIT_BASE_URL = 'http://localhost:8080'
+CONDUIT_API_ACTUATOR = f'{CONDUIT_BASE_URL}/actuator'
 
 
-def conduit_version():
-    logger.info(CONDUIT_API_URL_STATUS)
-    resp = requests.get(CONDUIT_API_URL_STATUS)
+def conduit_info():
+    url = f'{CONDUIT_API_ACTUATOR}/info'
+    logger.info(url)
+    resp = requests.get(url)
     logger.info(resp)
     return resp.status_code, resp.json()
 
 
-def conduit_availability():
-    logger.info(CONDUIT_API_URL_AVAILABILITY)
-    resp = requests.get(CONDUIT_API_URL_AVAILABILITY)
+def conduit_health():
+    url = f'{CONDUIT_API_ACTUATOR}/health'
+    logger.info(url)
+    resp = requests.get(url)
     logger.info(resp)
     return resp.status_code, resp.json()
--- conduitqe/tests/api/v1/test_sanity.py
+++ conduitqe/tests/api/v1/test_sanity.py
@@ -7,13 +7,13 @@ from conduitqe import api
 logger = logging.getLogger(__name__)
 
 
-def test_status_version():
-    status, resp = api.conduit_version()
+def test_conduit_version():
+    status, resp = api.conduit_info()
     assert status == 200
-    assert resp['version'] == '1.0.0'
+    assert resp['build']['version'] == '1.0.0'
 
 
-def test_status_availability():
-    status, resp = api.conduit_availability()
+def test_conduit_health():
+    status, resp = api.conduit_health()
     assert status == 200
-    assert resp['availability'] == 'OK'
+    assert resp['status'] == 'UP'
