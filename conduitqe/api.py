de0a4bac58ef6a7cc3f701b1424127729b9ec1fa
Rudá Moura <rmoura@redhat.com>
2019-05-16 13:46:18 -0300

    Add api.conduit_host_inventory(org_key)

--- conduitqe/api.py
+++ conduitqe/api.py
@@ -3,11 +3,16 @@
 import logging
 
 import requests
-from conduitqe.config import get_conduit_api_actuator
+from conduitqe.config import (
+        get_conduit_api_actuator,
+        get_conduit_api_inventory
+)
+from conduitqe.utils import create_authentication
 
 logger = logging.getLogger(__name__)
 
 CONDUIT_API_ACTUATOR = get_conduit_api_actuator()
+CONDUIT_API_INVENTORY = get_conduit_api_inventory()
 
 
 def conduit_info():
@@ -22,5 +27,15 @@ def conduit_health():
     url = f'{CONDUIT_API_ACTUATOR}/health'
     logger.info(url)
     resp = requests.get(url)
+    logger.info(resp)
+    return resp.status_code, resp.json()
+
+
+def conduit_host_inventory(org_key):
+    auth = create_authentication(org_key)
+    headers = {'x-rh-identity': auth}
+    url = f'{CONDUIT_API_INVENTORY}/hosts'
+    logger.info(url)
+    resp = requests.get(url, headers=headers)
     logger.info(resp)
     return resp.status_code, resp.json()
--- conduitqe/config.py
+++ conduitqe/config.py
@@ -3,13 +3,24 @@
 import os
 
 CONDUIT_BASE_URL = 'http://localhost:8080'
+INVENTORY_BASE_URL = 'http://insights-inventory.platform-qa.svc:8080'
 
 
 def get_conduit_base_url():
     return os.getenv('CONDUIT_BASE_URL', CONDUIT_BASE_URL)
 
 
+def get_inventory_base_url():
+    return os.getenv('INVENTORY_BASE_URL', INVENTORY_BASE_URL)
+
+
 def get_conduit_api_actuator():
     base_url = get_conduit_base_url()
     api_actuator = f'{base_url}/actuator'
     return api_actuator
+
+
+def get_conduit_api_inventory():
+    base_url = get_inventory_base_url()
+    api_inventory = f'{base_url}/api/inventory/v1'
+    return api_inventory
