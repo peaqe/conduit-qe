54c8371680129c996b5215d67c5daff52c5ae874
Rudá Moura <rmoura@redhat.com>
2019-05-27 18:49:18 -0300

    Authentication argument is 'identity'

--- conduitqe/utils.py
+++ conduitqe/utils.py
@@ -7,7 +7,7 @@ import base64
 def create_authentication(account_number):
     if type(account_number) is int:
         account_number = str(account_number)
-    auth = {'identify': {'account_number': account_number}}
+    auth = {'identity': {'account_number': account_number}}
     auth = json.dumps(auth)
     auth = base64.standard_b64encode(auth.encode('utf8'))
     return auth
