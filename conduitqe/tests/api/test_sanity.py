b15c7af48534e894dcf50d92261283fb01e784fb
Rudá Moura <rmoura@redhat.com>
2019-06-16 21:34:03 -0300

    test_sanity: provide error messages for asserts

--- conduitqe/tests/api/test_sanity.py
+++ conduitqe/tests/api/test_sanity.py
@@ -9,14 +9,15 @@ logger = logging.getLogger(__name__)
 
 def test_conduit_version():
     status, resp = api.conduit_info()
-    assert status == 200
+    assert status == 200, f'Conduit response with error: {status}'
     version = resp['build']['version']
     logger.info(f'Conduit version reported is {version}')
     major, minor, patch_level = version.split('.')
-    assert major, minor == ('1', '0')
+    assert (major, minor) == ('1', '0'), \
+        f'Invalid version to test: {major}.{minor}'
 
 
 def test_conduit_health():
     status, resp = api.conduit_health()
-    assert status == 200
-    assert resp['status'] == 'UP'
+    assert status == 200, f'Conduit response with error: {status}'
+    assert resp['status'] == 'UP', f'Bad status health: {resp["status"]}'
