4ee762b06ad513f757d9cb93395e669448aef647
Rudá Moura <rmoura@redhat.com>
2019-07-16 12:06:47 -0300

    create_authentication() returns a string

--- conduitqe/utils.py
+++ conduitqe/utils.py
@@ -1,13 +1,20 @@
-"""Utilities for rhsm-conduit."""
+"""Utilities for using rhsm-conduit."""
 
 import json
 import base64
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 def create_authentication(account_number):
+    """Create authentication with account number."""
     if type(account_number) is int:
         account_number = str(account_number)
     auth = {'identity': {'account_number': account_number}}
     auth = json.dumps(auth)
     auth = base64.standard_b64encode(auth.encode('utf8'))
+    auth = auth.decode('utf8')
+    logger.debug(auth)
     return auth
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,6 +5,6 @@ from conduitqe import utils
 
 def test_authentication():
     account_number = 123456789
-    expected = b'eyJpZGVudGl0eSI6IHsiYWNjb3VudF9udW1iZXIiOiAiMTIzNDU2Nzg5In19'
+    expected = 'eyJpZGVudGl0eSI6IHsiYWNjb3VudF9udW1iZXIiOiAiMTIzNDU2Nzg5In19'
     auth = utils.create_authentication(account_number)
     assert auth == expected
